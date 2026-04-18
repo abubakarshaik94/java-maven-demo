4e6e7f70a3fec40fe70c49ff6bbb56fe38888866
abubakarshaik94 <shaikmohammedabubakar99@gmail.com>
2026-04-18 09:10:23 +0530

    Create AppTest.java

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    void testGreet() {
+        assertEquals("Hello, Abubakar!", App.greet("Abubakar"));
+    }
+}
